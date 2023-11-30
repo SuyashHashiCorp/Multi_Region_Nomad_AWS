dbf83184883b4da1a7c913eb6f0af637d7ce7c72
SuyashHashiCorp <92308220+SuyashHashiCorp@users.noreply.github.com>
2023-11-30 11:08:23 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
-output "apj_public_ip" {
-  value = [module.apj_instance.apj_tag_name, module.apj_instance.apj_public_ip]
+output "ind_public_ip" {
+  value = [module.ind_instance.ind_tag_name, module.ind_instance.ind_public_ip]
 }
 
-output "usa_public_ip" {
-  value = [module.usa_instance.usa_tag_name, module.usa_instance.usa_public_ip]
+output "emea_public_ip" {
+  value = [module.emea_instance.emea_tag_name, module.emea_instance.emea_public_ip]
 }
 
