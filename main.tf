a6af391e390cd5fe5e628dcc39a27917ece173fa
SuyashHashiCorp <92308220+SuyashHashiCorp@users.noreply.github.com>
2023-11-30 11:08:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
-module "apj_instance" {
+module "ind_instance" {
   source = "/Users/suyash/Projects/Multi_Region_Nomad_AWS/ind_dc1_nomad_region"
 }
 
-module "usa_instance" {
+module "emea_instance" {
   source = "/Users/suyash/Projects/Multi_Region_Nomad_AWS/emea_dc2_nomad_region"
 }
